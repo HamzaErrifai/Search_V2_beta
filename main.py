445228d59d013d2bddd75e880dc89e1336d56260
Hamza Errifai <hamzaerrifai@hotmail.com>
2020-12-18 12:24:03 +0100

    final version

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
         MAX_Width = 70
         # MAX_HEIGHT = 40
         # line_count = 0
-        # set the size of the window
+        # # set the size of the window
         # with open(str(pathlib.Path(__file__).parent.absolute())+"\\data\\data.json") as file:
         #     for line in file:
         #         if line != "\n":
@@ -18,6 +18,7 @@ def main():
 
         # set the size of the window
         # os.system(f"mode con: cols={MAX_Width} lines={MAX_HEIGHT}")
+        os.system("cls")
         print("Search_Beta_V2".center(MAX_Width, "_").upper(), end='\n\n')
         print("Type '--help' for help\t'--exit' to exit", end='\n\n')
         History().show()  # Show the history
@@ -26,7 +27,7 @@ def main():
             word = input("Search >> ".upper())
         search = Search(word)
         return search.start()
-    except:
+    except :
         Alert("error", 'UNKNOWN ERROR ENCOUNTERED')
         return False
 
